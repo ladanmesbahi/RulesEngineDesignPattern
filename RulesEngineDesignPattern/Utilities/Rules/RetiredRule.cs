e0f6bccfb3862053f77a70bd673a926c2bc9a225
mesbahil <mesbahil@AG1115>
2023-04-29 15:35:47 +0330

    Implemented retired rule

---
+++ RulesEngineDesignPattern/Utilities/Rules/RetiredRule.cs
@@ -0,0 +1,13 @@
+﻿using RulesEngineDesignPattern.Entities;
+
+namespace RulesEngineDesignPattern.Utilities.Rules
+{
+    public class RetiredRule : ITaxCalculationRule
+    {
+        public async Task CalculateTax(TaxPayer taxPayer)
+        {
+            if (taxPayer.IsRetired)
+                taxPayer.TaxAmount = 0;
+        }
+    }
+}
